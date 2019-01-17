733bd94a47dfe81bfb93d41409d23bc965380d36
Adam <rumpelteam@gmail.com>
2019-01-17 21:57:29 +0100

    Clear project

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'jpk-client'
-  }
+export default {
+  name: 'jpk-client'
+}
 </script>
 
 <style>
-  /* CSS */
+	@import url('https://fonts.googleapis.com/css?family=Nunito:300,400,700');
+
+	* {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+	}
+
+	body {
+	  font-family: 'Nunito', sans-serif;
+	}
 </style>
---
+++ src/renderer/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="wrapper">
+    <main class="main">
+      <section>
+        JPK Client
+      </section>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home'
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
